a107c00c60ee0113a5bec704c7a660216b6c7a87
Connor Stack <connorstack123@gmail.com>
2024-06-07 21:31:24 -0500

    updating dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,7 @@
-val kotlin_version: String by project
-val logback_version: String by project
+val ktor_version = "2.3.0"
+val kotlin_version = "1.8.0"
+val logback_version = "1.2.11"
+val graphql_kotlin_version = "6.2.4"
 
 plugins {
     kotlin("jvm") version "2.0.0"
@@ -21,9 +23,12 @@ repositories {
 }
 
 dependencies {
-    implementation("io.ktor:ktor-server-core-jvm")
-    implementation("io.ktor:ktor-server-netty-jvm")
+    implementation("io.ktor:ktor-server-core-jvm:$ktor_version")
+    implementation("io.ktor:ktor-server-netty-jvm:$ktor_version")
     implementation("ch.qos.logback:logback-classic:$logback_version")
-    testImplementation("io.ktor:ktor-server-tests-jvm")
+    implementation("io.ktor:ktor-serialization-gson:$ktor_version")
+    implementation("com.expediagroup:graphql-kotlin-server:$graphql_kotlin_version")
+    implementation("com.expediagroup:graphql-kotlin-schema-generator:$graphql_kotlin_version")
+    testImplementation("io.ktor:ktor-server-tests-jvm:$ktor_version")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
 }
